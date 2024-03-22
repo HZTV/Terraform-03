861a42635aee686233288a3f743b9bbf285ae3e5
HZTV <149588305+HZTV@users.noreply.github.com>
2024-03-22 10:56:40 +0300

    Update count-vm.tf

--- count-vm.tf
+++ count-vm.tf
@@ -26,6 +26,7 @@ resource "yandex_compute_instance" "web" {
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
+    security_group_ids = [yandex_vpc_security_group.example.id] # Назначил группу безопасности
   }
 
   metadata = {
